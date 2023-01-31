039d9284e898a872da7bab26e5d80b6dba0a5d11
Rafael Lyra <rafael.lyra@lnls.br>
2023-01-31 13:07:41 -0300

    Pip package creation

---
+++ scripts/hw.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+
+"""
+ A Hello World script
+"""
+
+from pipver import helloworld
+
+if __name__ == "__main__":
+    helloworld.main()
+    helloworld.main()
+    helloworld.main()
---
+++ setup.py
@@ -0,0 +1,39 @@
+#!/usr/bin python
+
+from setuptools import setup
+import versioneer
+
+with open('LICENSE', 'r') as _f:
+    _license = _f.read()
+
+with open('README.rst', 'r') as _f:
+    _long_description = _f.read()
+
+with open('requirements.txt', 'r') as _f:
+    _requirements = _f.read().strip().split('\n')
+
+setup(
+    name="pipver",
+    version=versioneer.get_version(),
+    cmdclass=versioneer.get_cmdclass(),
+    description="Python versioning example",
+    long_description=_long_description,
+    url="http://pypi.python.org/pypi",
+    author="Rafael Lyra",
+    author_email="rafael.lyra@lnls.br",
+    license=_license,
+    packages=[
+        'pipver',
+        'test'],
+    scripts=[
+        "scripts/hw.py"
+    ],
+    install_requires=_requirements,
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'Intended Audience :: Science/Research',
+        'License :: None',
+        'Operating System :: Ubuntu :: Linux',
+        'Programming Language :: Python 2.7.17'
+    ]
+)
